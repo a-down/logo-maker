0ae38fdea0b6b70a6964c479a3f86f006f360d10
a-down <136649391+a2downing@users.noreply.github.com>
2023-07-27 16:10:42 -0500

    commented out process

---
+++ index.js
@@ -0,0 +1,28 @@
+/* 
+Prompts
+- start on command
+- enter up to 3 characters
+- ask for text color (color keyword or hex)
+- choose from list of shapes
+  - circle
+  - triangle
+  - square
+- ask for shape color
+*/
+
+// constructor uses the prompts responses to build object
+  // shapes constructor
+    // triangle with inheritance
+    // circle with inheritance
+    // square with inheritance
+  
+  // shapes is the global constructor
+    // use ${response.shapeChoice} to determine which class extension is used
+  
+  // renderLogo() is comprised of
+    // take in the object
+    // send to utility function
+      // put the keys from object into temperal literal
+      // return finished string
+    // fs.writeFile the logo.svg with the content of the finished string
+    // console.log(Generated logo.svg)
